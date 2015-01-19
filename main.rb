9f59cf2612fef15dab6c0e40e706f49b94769675
Chris <christopherdieckhaus@gmail.com>
2015-01-18 20:38:26 -0500

    Make souce file a parameter to the program

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 output_length = 1000
-file = 'text.txt'
+file = ARGV[0]
+puts file
 #Parse file and store words in dict
 dict = {}
 word_list = []
@@ -23,7 +24,7 @@ end
     dict[word_list[i]] = val
   end  
 end
-dict[word_list[-1]] = ["the"]
+dict[word_list[-1]] = ["the"]         #tmp bug fix for last word
 
 start_word = word_list[rand(0..word_list.length-1)]
 print start_word.capitalize
@@ -33,4 +34,4 @@ next_word = dict[start_word][rand(0...dict[start_word].length)]
   print " " + next_word
   next_word = dict[next_word][rand(0...dict[next_word].length)]
 end
-puts "."
+puts "."  
