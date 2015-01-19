25a492415559bb8cd3fc2394201fab21bd8837b3
Chris <christopherdieckhaus@gmail.com>
2015-01-18 19:46:57 -0500

    Program works for n=1

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
-
-word_dict = {}
+output_length = 1000
+file = 'text.txt'
+#Parse file and store words in dict
+dict = {}
 word_list = []
-File.open('text.txt', 'r') do |f1|  
+File.open(file, 'r') do |f1|  
   while line = f1.gets  
     words = line.split
     (words).each do |i|
@@ -11,15 +13,24 @@ File.open('text.txt', 'r') do |f1|
 end
 
 (0...word_list.length).each do |i|
-  if !word_dict.has_key?(word_list[i])      #if the word is not in the dict
+  if !dict.has_key?(word_list[i])      #if the word is not in the dict
     val = []
     val.push(word_list[i+1])
-    word_dict[word_list[i]] = val
+    dict[word_list[i]] = val
   else
-    val = word_dict[word_list[i]]
+    val = dict[word_list[i]]
     val.push(word_list[i+1])
-    word_dict[word_list[i]] = val
+    dict[word_list[i]] = val
   end  
 end
+dict[word_list[-1]] = ["the"]
 
-puts word_dict["the"][rand(0..10)]
+start_word = word_list[rand(0..word_list.length-1)]
+print start_word.capitalize
+next_word = dict[start_word][rand(0...dict[start_word].length)]
+
+(0..output_length).each do |x|
+  print " " + next_word
+  next_word = dict[next_word][rand(0...dict[next_word].length)]
+end
+puts "."
